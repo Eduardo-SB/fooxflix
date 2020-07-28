7d223e899d19f00540da5c0b77e746d380a1a72c
Eduardo SB <eduardosb95@gmail.com>
2020-07-27 23:24:39 -0300

    Change footer img

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import { FooterBase } from './styles';
+import { FooterBase, ImgFooter } from './styles';
 import LogoFoox from '../../assets/img/avatar.png';
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.fooxit.com.br/">
-        <img src={LogoFoox} alt="Logo FooxIT" />
+        <ImgFooter src={LogoFoox} alt="Logo FooxIT" />
       </a>
       <p>
         Criado com muito ❤️ 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
-
 export const FooterBase = styled.footer`
-  background: var(--black);
+  background: var(--white);
   border-top: 2px solid var(--primary);
   padding-left: 16px;
   padding-right: 16px;
-  padding-top: 32px;
-  padding-bottom: 32px;
-  color: var(--white);
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: var(--black);
   text-align: center;
   @media (max-width: 800px) {
     margin-bottom: 50px;
   }
+`;
+
+export const ImgFooter = styled.img`
+  height: 150px;
+  width: 250px
 `;
