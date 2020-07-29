a3c8ee4c46e8cc74c2d3e814feb0eb5cb30d9842
Eduardo SB <eduardosb95@gmail.com>
2020-07-28 23:17:11 -0300

    Initial input forms

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -7,8 +7,21 @@ export default function Video(){
   return (
     <>
       <PageDefault>
-          <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria</h1>
         
+        <form>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
         <Link to="/">
           Ir para home
         </Link>
